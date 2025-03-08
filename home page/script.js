5e0efba282a8ba0e3f5346df16a77140319e119c
Yasmine Emad <jasminezzat015@gmail.com>
2025-03-09 01:33:39 +0200

    update2

--- home page/script.js
+++ home page/script.js
@@ -1,12 +1,15 @@
 let backToTopBtn = document.getElementById("backToTopBtn");
+
+// Show/hide the back-to-top button
 window.onscroll = function() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         backToTopBtn.style.display = "block";
-    }
-    else {
+    } else {
         backToTopBtn.style.display = "none";
     }
 };
+
+// Smooth scroll to top
 backToTopBtn.onclick = function() {
     window.scrollTo({
         top: 0,
